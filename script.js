707a179bb1faee81ae98888665c469e1b55a9e7d
Fadhil Andriawan <fadhil@ilcs.co.id>
2025-03-24 14:51:49 +0700

    [UPDATE] redesign and logic

--- script.js
+++ script.js
@@ -9,19 +9,35 @@ const onShowNotification = () => {
     //     notifSection[0].classList.remove('show')
     // }
 
+    changePosition()
+
     const elementNotif = document.createElement('div')
-    elementNotif.className = 'notification-container'
-    elementNotif.textContent = 'Test append'
+    elementNotif.className = 'notification-container neobrutalism-shadow'
+    const notifText = document.createElement('h5')
+    notifText.textContent = 'Heading Notification'
+    elementNotif.appendChild(notifText)
     
-    notifSection[0].appendChild(
+    notifSection[0].prepend(
         elementNotif
     )
     
     setTimeout(() => {
         elementNotif.classList.add('show')
-    }, 100)
+    }, 50)
     setTimeout(() => {
         elementNotif.classList.remove('show')
-        // notifSection[0].removeChild(elementNotif)
-    }, 2000)
+        setTimeout(() => {
+            notifSection[0].removeChild(elementNotif)
+        }, 1000)
+    }, 3000)
+}
+
+const changePosition = () => {
+    const allNotif = document.querySelectorAll('.notification-container')
+
+    allNotif.forEach((el, idx) => {
+        console.log(el)
+        el.setAttribute("style", `transform: translate(0%, ${120 * (idx + 1)}%)`)
+    })
+    console.log("🚀 ~ changePosition ~ allNotif:", allNotif)
 }
